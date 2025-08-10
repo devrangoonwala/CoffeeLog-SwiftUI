a79447a8d1f9d62b6f5e54cd7846c38447bc858b
Dev Rangoonwala <60741127+devrangoonwala@users.noreply.github.com>
2025-08-10 20:07:57 +0530

    Initial Commit

---
+++ CoffeeLog/CoffeeLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeLogApp.swift
+//  CoffeeLog
+//
+//  Created by Dev on 10/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeeLog
+//
+//  Created by Dev on 10/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
